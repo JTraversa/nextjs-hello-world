1dd1fd595efe6a04f1d26c7812fcf04a7af1207e
Julian Traversa <unitedterrorhots@gmail.com>
2024-04-24 05:07:20 -0400

    footer

--- src/components/Layout/Footer/index.tsx
+++ src/components/Layout/Footer/index.tsx
@@ -6,30 +6,27 @@ const Footer: React.FC = () => {
 		<footer className="pt-12">
 			<div className="mx-auto w-full max-w-screen-lg border-t border-gray-700 px-4 py-12">
 				<div className="flex flex-col items-center justify-between gap-4 lg:flex-row">
-					<p className="text-sm text-gray-500">&copy; 2022, Goober. All rights reserved.</p>
+					<p className="text-sm text-gray-500">&copy; 2024, Warlock Labs. All rights reserved.</p>
 					<div className="flex gap-4 text-gray-600">
-						<a className="hover:text-[#8EF42E] hover:underline" href="https://docs.goober.xyz/" target="_blank" rel="noreferrer">
+						<a
+							className="transition-colors hover:text-[#818CF8] hover:underline hover:shadow-inherit"
+							href="https://docs.warlock.xyz/"
+							target="_blank"
+							rel="noreferrer"
+						>
 							Docs
 						</a>
 						<a
-							className="hover:text-[#8EF42E] hover:underline"
-							href="https://etherscan.io/address/0x2275d4937b6bfd3c75823744d3efbf6c3a8de473"
+							className="transition-colors hover:text-[#818CF8] hover:underline hover:shadow-inherit"
+							href="/litepaper"
 							target="_blank"
 							rel="noreferrer"
 						>
-							Etherscan
+							Litepaper
 						</a>
 						<a
-							className="transition-colors hover:text-[#8EF42E] hover:underline"
-							href="https://github.com/gooberxyz/goobervault"
-							target="_blank"
-							rel="noreferrer"
-						>
-							Github
-						</a>
-						<a
-							className="transition-colors hover:text-[#8EF42E] hover:underline"
-							href="https://twitter.com/goober_xyz"
+							className="transition-colors hover:text-[#818CF8] hover:underline hover:shadow-inherit"
+							href="https://twitter.com/warlock_xyz"
 							target="_blank"
 							rel="noreferrer"
 						>
